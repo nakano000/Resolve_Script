166930d00fec18dbe25c5cd23e5615927dd6c5cb
Yoshio Nakano <yoshio_nakano@mtb.biglobe.ne.jp>
2022-07-08 15:20:23 +0900

    UI調整

--- library/python/rs/tool/resolve_launcher/resolve_launcher_ui.py
+++ library/python/rs/tool/resolve_launcher/resolve_launcher_ui.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'C:\Users\yoshi\PycharmProjects\Resolve_Script\library\python\rs\tool\resolve_launcher\resolve_launcher.ui',
 # licensing of 'C:\Users\yoshi\PycharmProjects\Resolve_Script\library\python\rs\tool\resolve_launcher\resolve_launcher.ui' applies.
 #
-# Created: Fri Jul  8 13:04:09 2022
+# Created: Fri Jul  8 14:59:02 2022
 #      by: pyside2-uic  running on PySide2 5.12.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -13,33 +13,33 @@ from PySide2 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(558, 120)
+        MainWindow.resize(674, 133)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.verticalLayout.setObjectName("verticalLayout")
         self.gridLayout = QtWidgets.QGridLayout()
         self.gridLayout.setObjectName("gridLayout")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setObjectName("label")
-        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
-        self.fusionLineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.fusionLineEdit.setObjectName("fusionLineEdit")
-        self.gridLayout.addWidget(self.fusionLineEdit, 0, 1, 1, 1)
-        self.fusionToolButton = QtWidgets.QToolButton(self.centralwidget)
-        self.fusionToolButton.setObjectName("fusionToolButton")
-        self.gridLayout.addWidget(self.fusionToolButton, 0, 2, 1, 1)
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setObjectName("label_2")
-        self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+        self.gridLayout.addWidget(self.label_2, 0, 0, 1, 1)
         self.resolveLineEdit = QtWidgets.QLineEdit(self.centralwidget)
         self.resolveLineEdit.setObjectName("resolveLineEdit")
-        self.gridLayout.addWidget(self.resolveLineEdit, 1, 1, 1, 1)
+        self.gridLayout.addWidget(self.resolveLineEdit, 0, 1, 1, 1)
         self.resolveToolButton = QtWidgets.QToolButton(self.centralwidget)
         self.resolveToolButton.setObjectName("resolveToolButton")
-        self.gridLayout.addWidget(self.resolveToolButton, 1, 2, 1, 1)
+        self.gridLayout.addWidget(self.resolveToolButton, 0, 2, 1, 1)
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 1, 0, 1, 1)
+        self.fusionLineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.fusionLineEdit.setObjectName("fusionLineEdit")
+        self.gridLayout.addWidget(self.fusionLineEdit, 1, 1, 1, 1)
+        self.fusionToolButton = QtWidgets.QToolButton(self.centralwidget)
+        self.fusionToolButton.setObjectName("fusionToolButton")
+        self.gridLayout.addWidget(self.fusionToolButton, 1, 2, 1, 1)
         self.verticalLayout.addLayout(self.gridLayout)
-        spacerItem = QtWidgets.QSpacerItem(20, 0, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        spacerItem = QtWidgets.QSpacerItem(653, 10, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.verticalLayout.addItem(spacerItem)
         self.horizontalLayout = QtWidgets.QHBoxLayout()
         self.horizontalLayout.setObjectName("horizontalLayout")
@@ -67,10 +67,10 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QtWidgets.QApplication.translate("MainWindow", "MainWindow", None, -1))
-        self.label.setText(QtWidgets.QApplication.translate("MainWindow", "Fusion", None, -1))
-        self.fusionToolButton.setText(QtWidgets.QApplication.translate("MainWindow", "...", None, -1))
         self.label_2.setText(QtWidgets.QApplication.translate("MainWindow", "Resolve", None, -1))
         self.resolveToolButton.setText(QtWidgets.QApplication.translate("MainWindow", "...", None, -1))
+        self.label.setText(QtWidgets.QApplication.translate("MainWindow", "Fusion", None, -1))
+        self.fusionToolButton.setText(QtWidgets.QApplication.translate("MainWindow", "...", None, -1))
         self.resolveButton.setText(QtWidgets.QApplication.translate("MainWindow", "Resolve", None, -1))
         self.fusionButton.setText(QtWidgets.QApplication.translate("MainWindow", "Fusion", None, -1))
         self.closeButton.setText(QtWidgets.QApplication.translate("MainWindow", "close", None, -1))
