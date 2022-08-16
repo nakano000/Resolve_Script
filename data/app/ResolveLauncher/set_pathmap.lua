86616507da35c6ff47593f721dbc2b53673e2c96
Yoshio Nakano <yoshio_nakano@mtb.biglobe.ne.jp>
2022-08-16 18:35:45 +0900

    pathmapの設定を保存出来てないバグを修正

--- data/app/ResolveLauncher/set_pathmap.lua
+++ data/app/ResolveLauncher/set_pathmap.lua
@@ -1,6 +1,7 @@
 local user_path = app:GetPrefs("Global.Paths.Map.UserPaths:")
-if not user_path:find("$(RS_FUSION_USER_PATH)") then
+if not user_path:find("%$%(RS_FUSION_USER_PATH%)") then
     user_path = user_path .. ";$(RS_FUSION_USER_PATH)"
+    app:SetPrefs("Global.Paths.Map.UserPaths:", user_path)
+    app:SavePrefs()
 end
-app:SetPrefs("Global.Paths.Map.UserPaths:", user_path)
 print("Global.Paths.Map.UserPaths: " .. user_path)
