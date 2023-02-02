fc5b7db4ddb9486050e3636cbc20523c5d6cfbca
Yoshio Nakano <yoshio_nakano@mtb.biglobe.ne.jp>
2023-02-03 03:19:17 +0900

    1.6.14

--- library/python/rs/tool/launcher.py
+++ library/python/rs/tool/launcher.py
@@ -23,7 +23,7 @@ from rs.gui import appearance
 from rs.gui.script_button import ScriptButton
 
 APP_NAME = 'りぞりぷと'
-__version__ = '1.6.13'
+__version__ = '1.6.14'
 
 MENU_JSON = config.ROOT_PATH.joinpath('data', 'app', 'launcher_menu.json')
 
