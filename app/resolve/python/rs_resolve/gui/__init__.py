75026e0c41c58d39492bacdb16a404130e4e93c3
Yoshio Nakano <yoshio_nakano@mtb.biglobe.ne.jp>
2023-09-24 06:23:16 +0900

    macではpywinctlを使わないように変更

--- app/resolve/python/rs_resolve/gui/__init__.py
+++ app/resolve/python/rs_resolve/gui/__init__.py
@@ -1,20 +1,21 @@
 import subprocess
 
-import pywinctl
-
 from rs.core import util
 
+if util.IS_MAC:
+    def get_resolve_window(pj_name):
+        return None
+else:
+    import pywinctl
 
-def get_resolve_window(pj_name):
-    for t in pywinctl.getAllTitles():
-        if not util.IS_MAC:
+
+    def get_resolve_window(pj_name):
+        for t in pywinctl.getAllTitles():
             flag = t.startswith('DaVinci Resolve') and t.endswith(pj_name)
             flag = flag or t.startswith('DaVinci Resolve by Blackmagic Design')
-        else:
-            flag = t == pj_name
-        if flag:
-            return pywinctl.getWindowsWithTitle(t)[0]
-    return None
+            if flag:
+                return pywinctl.getWindowsWithTitle(t)[0]
+        return None
 
 
 def activate_window(w):
