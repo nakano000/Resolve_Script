b67608789a33434ed81d2058b9d6ce914778a97d
Yoshio Nakano <yoshio_nakano@mtb.biglobe.ne.jp>
2025-03-12 17:07:11 +0900

    2.5.13

--- library/python/rs/tool/launcher.py
+++ library/python/rs/tool/launcher.py
@@ -35,7 +35,7 @@ from rs.gui.script_button import ScriptButton
 
 APP_NAME = 'りぞりぷと'
 APP_NAME_EN = 'RIZORIPUTO'
-__version__ = '2.5.12'
+__version__ = '2.5.13'
 
 MENU_JSON = config.ROOT_PATH.joinpath('data', 'app', 'launcher_menu.json')
 
